d642bb7f0fea34a93c90b594fc2b0b37517ae9a3
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2020-02-26 05:47:17 -0300

    Fixing issues

--- scripts/open_utdf_all_days.py
+++ scripts/open_utdf_all_days.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 file1 = pd.read_csv('files_to_process/file1.csv')
 file2 = pd.read_csv('files_to_process/file2.csv')
-UTdf = pd.merge(file1, file2, how='outer', left_index=True, right_index=True, on='datetime', suffixes=('_file1', '_file2'))
+UTdf = file1.merge( file2, how='outer',  on='datetime', suffixes=('_file1', '_file2'))
 UTdf.set_index('datetime', inplace=True, drop=True)
 UTdf.index = pd.to_datetime(UTdf.index)
 print(UTdf.columns)
